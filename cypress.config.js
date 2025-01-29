9252bf81daa24e9ea9a1447a389f2e5cc819b813
saurav kushwaha <sauravkushwaha2219@gmail.com>
2025-01-29 15:23:13 +0530

    added project id and key

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'gk4bx6',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
@@ -11,3 +12,4 @@ module.exports = defineConfig({
     "url":"https://rahulshettyacademy.com/angularpractice/"
   },
 });
+//npx cypress run --record --key 293dbe56-1cb2-4ac9-bd03-38e4f99271da
