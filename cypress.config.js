29bb82c232e8651c8a758d8b05ec05810d7711dc
saurav kushwaha <sauravkushwaha2219@gmail.com>
2025-01-29 17:19:15 +0530

    added retries to run test again package file

--- cypress.config.js
+++ cypress.config.js
@@ -12,5 +12,8 @@ module.exports = defineConfig({
     // set global environment variables
     "url":"https://rahulshettyacademy.com/angularpractice/"
   },
+  // it used to run the failed test cases again in screenshot can see 
+  retries: {
+     runMode: 1 },
 });
 //npx cypress run --record --key 293dbe56-1cb2-4ac9-bd03-38e4f99271da
